86948bc8b1c59a1618cc3d1b2c42d7f7f1925e46
Vince Foley <vincefoley@gmail.com>
2018-10-13 18:17:15 -0700

    Initial commit

---
+++ lib/new_relic_ecto.ex
@@ -0,0 +1,18 @@
+defmodule NewRelicEcto do
+  @moduledoc """
+  Documentation for NewRelicEcto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NewRelicEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/new_relic_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule NewRelicEctoTest do
+  use ExUnit.Case
+  doctest NewRelicEcto
+
+  test "greets the world" do
+    assert NewRelicEcto.hello() == :world
+  end
+end
