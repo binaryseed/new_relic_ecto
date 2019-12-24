4a7e637b619913deb8af6759a5e4c77a60f8ed03
Vince Foley <vincefoley@gmail.com>
2019-12-24 00:02:36 -0800

    report span events

--- lib/new_relic/ecto/telemetry.ex
+++ lib/new_relic/ecto/telemetry.ex
@@ -42,7 +42,7 @@ defmodule NewRelic.Ecto.Telemetry do
 
   defp extract_repo_configs(otp_app, ecto_repos) do
     Enum.into(ecto_repos, %{}, fn repo ->
-      {repo, Application.get_env(otp_app, repo)}
+      {repo, Application.get_env(otp_app, repo) |> Map.new()}
     end)
   end
 
@@ -65,36 +65,34 @@ defmodule NewRelic.Ecto.Telemetry do
   def handle_event(
         _event,
         %{query_time: duration_ns},
-        %{type: :ecto_sql_query, repo: repo} = metadata,
+        %{type: :ecto_sql_query, repo: repo, query: query} = metadata,
         config
       ) do
-    pid = inspect(self())
     end_time = System.system_time(:millisecond)
-
     duration_ms = System.convert_time_unit(duration_ns, :nanosecond, :millisecond)
     duration_s = duration_ms / 1000
-
     start_time = end_time - duration_ms
-    repo_config = config.repo_configs[repo]
 
+    %{hostname: hostname, port: port, database: database} = config.repo_configs[repo]
+
+    pid = inspect(self())
     id = {:ecto_sql_query, make_ref()}
     parent_id = Process.get(:nr_current_span) || :root
 
     with {datastore, table, operation} <- parse_ecto_metadata(metadata) do
-      table_name = "#{inspect(repo)}.#{table}"
-      primary_name = "#{datastore} #{table_name} #{operation}"
-      secondary_name = "#{repo_config[:hostname]}:#{repo_config[:port]}/#{repo_config[:database]}"
+      metric_name = "Datastore/statement/#{datastore}/#{table}/#{operation}"
+      secondary_name = "#{inspect(repo)} #{hostname}:#{port}/#{database}"
 
       NewRelic.Transaction.Reporter.add_trace_segment(%{
-        primary_name: primary_name,
+        primary_name: metric_name,
         secondary_name: secondary_name,
         attributes: %{
-          sql: metadata.query,
+          sql: query,
           collection: table,
           operation: operation,
-          host: repo_config[:hostname],
-          database_name: repo_config[:database],
-          port_path_or_id: repo_config[:port]
+          host: hostname,
+          database_name: database,
+          port_path_or_id: port
         },
         pid: pid,
         id: id,
@@ -103,8 +101,27 @@ defmodule NewRelic.Ecto.Telemetry do
         end_time: end_time
       })
 
+      NewRelic.report_span(
+        timestamp_ms: start_time,
+        duration_s: duration_s,
+        name: metric_name,
+        edge: [span: id, parent: parent_id],
+        category: "datastore",
+        attributes: %{
+          component: datastore,
+          "span.kind": :client,
+          "db.statement": query,
+          "db.instance": database,
+          "peer.address": "#{hostname}:#{port}}",
+          "peer.hostname": hostname,
+          "ecto.repo": inspect(repo),
+          "db.table": table,
+          "db.operation": operation
+        }
+      )
+
       NewRelic.report_metric(
-        {:datastore, datastore, table_name, operation},
+        {:datastore, datastore, table, operation},
         duration_s: duration_s
       )
 
@@ -113,8 +130,8 @@ defmodule NewRelic.Ecto.Telemetry do
         databaseDuration: duration_s,
         datastore_call_count: 1,
         datastore_duration_ms: duration_ms,
-        "datastore.#{table_name}.call_count": 1,
-        "datastore.#{table_name}.duration_ms": duration_ms
+        "datastore.#{table}.call_count": 1,
+        "datastore.#{table}.duration_ms": duration_ms
       )
     end
   end
